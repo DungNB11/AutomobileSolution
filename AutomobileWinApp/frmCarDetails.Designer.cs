71b27591bea7f485d16d0da4e6dbebfac47329f6
DungNB <nbdung0000@gmail.com>
2024-01-19 08:07:18 +0700

    Add project files.

---
+++ AutomobileWinApp/frmCarDetails.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AutomobileWinApp
+{
+    partial class frmCarDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbCarID = new System.Windows.Forms.Label();
+            lbCarName = new System.Windows.Forms.Label();
+            lbManufacturer = new System.Windows.Forms.Label();
+            lbPrice = new System.Windows.Forms.Label();
+            lbReleaseYear = new System.Windows.Forms.Label();
+            txtCarID = new System.Windows.Forms.TextBox();
+            txtCarName = new System.Windows.Forms.TextBox();
+            txtReleaseYear = new System.Windows.Forms.MaskedTextBox();
+            txtPrice = new System.Windows.Forms.MaskedTextBox();
+            cboManufacturer = new System.Windows.Forms.ComboBox();
+            btnSave = new System.Windows.Forms.Button();
+            btnCancel = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // lbCarID
+            // 
+            lbCarID.AutoSize = true;
+            lbCarID.Location = new System.Drawing.Point(57, 46);
+            lbCarID.Name = "lbCarID";
+            lbCarID.Size = new System.Drawing.Size(50, 20);
+            lbCarID.TabIndex = 0;
+            lbCarID.Text = "Car ID";
+            lbCarID.Click += lbCarID_Click;
+            // 
+            // lbCarName
+            // 
+            lbCarName.AutoSize = true;
+            lbCarName.Location = new System.Drawing.Point(57, 93);
+            lbCarName.Name = "lbCarName";
+            lbCarName.Size = new System.Drawing.Size(75, 20);
+            lbCarName.TabIndex = 1;
+            lbCarName.Text = "Car Name";
+            // 
+            // lbManufacturer
+            // 
+            lbManufacturer.AutoSize = true;
+            lbManufacturer.Location = new System.Drawing.Point(57, 150);
+            lbManufacturer.Name = "lbManufacturer";
+            lbManufacturer.Size = new System.Drawing.Size(97, 20);
+            lbManufacturer.TabIndex = 2;
+            lbManufacturer.Text = "Manufacturer";
+            // 
+            // lbPrice
+            // 
+            lbPrice.AutoSize = true;
+            lbPrice.Location = new System.Drawing.Point(57, 205);
+            lbPrice.Name = "lbPrice";
+            lbPrice.Size = new System.Drawing.Size(41, 20);
+            lbPrice.TabIndex = 3;
+            lbPrice.Text = "Price";
+            // 
+            // lbReleaseYear
+            // 
+            lbReleaseYear.AutoSize = true;
+            lbReleaseYear.Location = new System.Drawing.Point(57, 269);
+            lbReleaseYear.Name = "lbReleaseYear";
+            lbReleaseYear.Size = new System.Drawing.Size(92, 20);
+            lbReleaseYear.TabIndex = 4;
+            lbReleaseYear.Text = "Release Year";
+            // 
+            // txtCarID
+            // 
+            txtCarID.Location = new System.Drawing.Point(176, 46);
+            txtCarID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtCarID.Name = "txtCarID";
+            txtCarID.Size = new System.Drawing.Size(219, 27);
+            txtCarID.TabIndex = 5;
+            // 
+            // txtCarName
+            // 
+            txtCarName.Location = new System.Drawing.Point(176, 93);
+            txtCarName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtCarName.Name = "txtCarName";
+            txtCarName.Size = new System.Drawing.Size(219, 27);
+            txtCarName.TabIndex = 6;
+            // 
+            // txtReleaseYear
+            // 
+            txtReleaseYear.Location = new System.Drawing.Point(176, 269);
+            txtReleaseYear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtReleaseYear.Mask = "0000";
+            txtReleaseYear.Name = "txtReleaseYear";
+            txtReleaseYear.Size = new System.Drawing.Size(219, 27);
+            txtReleaseYear.TabIndex = 7;
+            txtReleaseYear.Text = "0";
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new System.Drawing.Point(176, 205);
+            txtPrice.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtPrice.Mask = "000000000";
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new System.Drawing.Size(219, 27);
+            txtPrice.TabIndex = 8;
+            txtPrice.Text = "0";
+            // 
+            // cboManufacturer
+            // 
+            cboManufacturer.FormattingEnabled = true;
+            cboManufacturer.Items.AddRange(new object[] { "Audi", "BMW", "Ford", "Honda", "Hyundai", "Kia", "Suzuki", "Toyota" });
+            cboManufacturer.Location = new System.Drawing.Point(176, 150);
+            cboManufacturer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            cboManufacturer.Name = "cboManufacturer";
+            cboManufacturer.Size = new System.Drawing.Size(219, 28);
+            cboManufacturer.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            btnSave.Location = new System.Drawing.Point(176, 348);
+            btnSave.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(71, 31);
+            btnSave.TabIndex = 10;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.DialogResult = System.Windows.Forms.DialogResult.OK;
+            btnCancel.Location = new System.Drawing.Point(324, 348);
+            btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new System.Drawing.Size(71, 31);
+            btnCancel.TabIndex = 11;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // frmCarDetails
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(458, 427);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(cboManufacturer);
+            Controls.Add(txtPrice);
+            Controls.Add(txtReleaseYear);
+            Controls.Add(txtCarName);
+            Controls.Add(txtCarID);
+            Controls.Add(lbReleaseYear);
+            Controls.Add(lbPrice);
+            Controls.Add(lbManufacturer);
+            Controls.Add(lbCarName);
+            Controls.Add(lbCarID);
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "frmCarDetails";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "frmCarDetails";
+            Load += frmCarDetails_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCarID;
+        private System.Windows.Forms.Label lbCarName;
+        private System.Windows.Forms.Label lbManufacturer;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label lbReleaseYear;
+        private System.Windows.Forms.TextBox txtCarID;
+        private System.Windows.Forms.TextBox txtCarName;
+        private System.Windows.Forms.MaskedTextBox txtReleaseYear;
+        private System.Windows.Forms.MaskedTextBox txtPrice;
+        private System.Windows.Forms.ComboBox cboManufacturer;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
